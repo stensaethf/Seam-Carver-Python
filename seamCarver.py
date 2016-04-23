ba154bd0dbec2778e4622c211f3f1f462f1fec97
Frederik Stensaeth <stensaethf@carleton.edu>
2016-04-22 22:02:11 -0500

    inital setup of carve and main

---
+++ seamCarver.py
@@ -0,0 +1,20 @@
+"""
+seamCarver.py
+Frederik Roenn Stensaeth
+04.22.16
+
+Python implementation of a content-aware image resizing algorithm.
+"""
+
+def carve(filename, seam_num, direction):
+	print 'Running carve'
+	print 'File: ' + filename
+	print 'Number of seams: ' + seam_num
+	print 'Direction: ' + direction
+
+
+def main():
+	print 'Running Main()'
+
+if __name__ == '__main__':
+	main()
