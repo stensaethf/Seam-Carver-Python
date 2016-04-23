85bd5753989d47177c17447271043b2be29cd7b3
Frederik Stensaeth <stensaethf@carleton.edu>
2016-04-22 22:05:07 -0500

    computeEnergy, findSeam, removeSeam, showImage functions

--- seamCarver.py
+++ seamCarver.py
@@ -6,15 +6,27 @@ Frederik Roenn Stensaeth
 Python implementation of a content-aware image resizing algorithm.
 """
 
-def carve(filename, seam_num, direction):
-	print 'Running carve'
+def carve(image, seam_num, direction):
+	print 'Running: carve()'
 	print 'File: ' + filename
 	print 'Number of seams: ' + seam_num
 	print 'Direction: ' + direction
 
+def computeEnergy(image):
+	print 'Running: computeEnergy()'
+
+def findSeam(energy, direction):
+	print 'Running: findSeam()'
+
+def removeSeam(image, seam, direction):
+	print 'Running: removeSeam()'
+
+def showImage(image):
+	print 'Running: showImage'
+
 
 def main():
-	print 'Running Main()'
+	print 'Running: Main()'
 
 if __name__ == '__main__':
 	main()
