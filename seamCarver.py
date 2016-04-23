cf1ca09c33efddc39da4297c0ea2541cfc3db294
Frederik Stensaeth <stensaethf@carleton.edu>
2016-04-22 22:09:44 -0500

    carveSeam added

--- seamCarver.py
+++ seamCarver.py
@@ -6,27 +6,43 @@ Frederik Roenn Stensaeth
 Python implementation of a content-aware image resizing algorithm.
 """
 
-def carve(image, seam_num, direction):
+def carveSeams(image, seam_num, direction):
 	print 'Running: carve()'
 	print 'File: ' + filename
 	print 'Number of seams: ' + seam_num
 	print 'Direction: ' + direction
 
+	# code.
+
+def carveSeam(image, direction):
+	print 'Running: carseSeam()'
+
+	# code.
+
 def computeEnergy(image):
 	print 'Running: computeEnergy()'
+
+	# code.
 
 def findSeam(energy, direction):
 	print 'Running: findSeam()'
 
+	# code.
+
 def removeSeam(image, seam, direction):
 	print 'Running: removeSeam()'
+
+	# code.
 
 def showImage(image):
 	print 'Running: showImage'
 
+	# code.
 
 def main():
 	print 'Running: Main()'
+
+	# code.
 
 if __name__ == '__main__':
 	main()
